284fdcf283d0235be42c511b5ab9e88d2efb2495
BakayDev <gamempvs@gmail.com>
2019-08-18 00:55:24 +0600

    Main page customization

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MainLayoutComponent } from './chared/components/main-layout/main-layout.component';
+import { MainLayoutComponent } from './shared/components/main-layout/main-layout.component';
 import { PostPageComponent } from './post-page/post-page.component';
 import { HomePageComponent } from './home-page/home-page.component';
+import { PostComponent } from './shared/components/post/post.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainLayoutComponent,
     PostPageComponent,
-    HomePageComponent
+    HomePageComponent,
+    PostComponent,
   ],
   imports: [
     BrowserModule,
