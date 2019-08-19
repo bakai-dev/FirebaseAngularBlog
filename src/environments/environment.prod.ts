7fa1c13ab52132d69abfd86ebeee6f61fa6fd66d
BakayDev <gamempvs@gmail.com>
2019-08-19 21:50:07 +0600

    Firebase settings

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
-export const environment = {
-  production: true
+import {Environment} from "./interface";
+
+export const environment: Environment = {
+  production: true,
+  apiKey: 'AIzaSyBT38HYIpYXJ24uBl67cDRU4j-cWkOFS9s'
 };
