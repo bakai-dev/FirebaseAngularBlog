0a0512807f8d8652e623038861ed706caf6f071d
BakayDev <gamempvs@gmail.com>
2019-08-22 03:10:31 +0600

    Added router guard

--- src/app/admin/login-page/login-page.component.ts
+++ src/app/admin/login-page/login-page.component.ts
@@ -2,7 +2,7 @@ import {Component, OnInit} from '@angular/core'
 import {FormControl, FormGroup, Validators} from '@angular/forms';
 import {User} from '../../shared/interfaces';
 import {AuthService} from '../shared/services/auth.service';
-import {Router} from '@angular/router';
+import {ActivatedRoute, Params, Router} from '@angular/router';
 
 @Component({
     selector: 'app-login-page',
@@ -12,14 +12,24 @@ import {Router} from '@angular/router';
 export class LoginPageComponent implements OnInit {
 
     form: FormGroup
+    submitted = false
+    message: string
 
     constructor(
-        private auth: AuthService,
-        private router: Router
+        public auth: AuthService,
+        private router: Router,
+        private route: ActivatedRoute
     ) {
     }
 
     ngOnInit() {
+
+        this.route.queryParams.subscribe((params: Params)=>{
+            if (params['loginAgain']) {
+                this.message = 'Пожалуйста, введите данные'
+            }
+        })
+
         this.form = new FormGroup({
             email: new FormControl(null, [
                 Validators.required,
@@ -36,6 +46,7 @@ export class LoginPageComponent implements OnInit {
         if (this.form.invalid) {
             return
         }
+        this.submitted = true
 
         const user: User = {
             email: this.form.value.email,
@@ -45,7 +56,11 @@ export class LoginPageComponent implements OnInit {
         this.auth.login(user).subscribe(() => {
             this.form.reset()
             this.router.navigate(['/admin', 'dashboard'])
+            this.submitted = false
+        },()=>{
+            this.submitted = false
         })
+
     }
 }
 
