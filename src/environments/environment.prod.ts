e3c628791faa5aee92f23adac3e4d5719ff1df70
BakayDev <gamempvs@gmail.com>
2019-08-23 21:17:58 +0600

    Create interceptor for auth

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,6 @@ import {Environment} from "./interface";
 
 export const environment: Environment = {
   production: true,
-  apiKey: 'AIzaSyBT38HYIpYXJ24uBl67cDRU4j-cWkOFS9s'
+  apiKey: 'AIzaSyBT38HYIpYXJ24uBl67cDRU4j-cWkOFS9s',
+  fbDbUrl: 'https://strategic-pier-232608.firebaseio.com'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,8 @@ import {Environment} from "./interface";
 
 export const environment: Environment = {
   production: false,
-  apiKey: 'AIzaSyBT38HYIpYXJ24uBl67cDRU4j-cWkOFS9s'
+  apiKey: 'AIzaSyBT38HYIpYXJ24uBl67cDRU4j-cWkOFS9s',
+  fbDbUrl: 'https://strategic-pier-232608.firebaseio.com'
 };
 
 /*
