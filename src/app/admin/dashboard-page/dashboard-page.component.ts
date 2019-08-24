7ad3eb761cfaa7796fb136e2b20753bb319755e4
BakayDev <gamempvs@gmail.com>
2019-08-24 19:41:50 +0600

    Added Delete post

--- src/app/admin/dashboard-page/dashboard-page.component.ts
+++ src/app/admin/dashboard-page/dashboard-page.component.ts
@@ -2,6 +2,7 @@ import {Component, OnDestroy, OnInit} from '@angular/core';
 import {PostsService} from "../../shared/posts.service";
 import {Post} from "../../shared/interfaces";
 import {Subscription} from "rxjs";
+import {post} from "selenium-webdriver/http";
 
 @Component({
   selector: 'app-dashboard-page',
@@ -12,6 +13,7 @@ export class DashboardPageComponent implements OnInit, OnDestroy {
 
   posts: Post[] = []
   pSub: Subscription
+  dSub: Subscription
   searchStr : string = ''
 
   constructor(
@@ -28,10 +30,15 @@ export class DashboardPageComponent implements OnInit, OnDestroy {
     if (this.pSub) {
       this.pSub.unsubscribe()
     }
+    if (this.dSub) {
+      this.dSub.unsubscribe()
+    }
   }
 
-    remove(id : string) {
-
-    }
+  remove(id : string) {
+    this.dSub = this.postService.remove(id).subscribe(() => {
+      this.posts = this.posts.filter(post => post.id !== id)
+    })
+  }
 
 }
--- src/app/admin/edit-page/edit-page.component.ts
+++ src/app/admin/edit-page/edit-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute, Params} from "@angular/router";
+import {PostsService} from "../../shared/posts.service";
 
 @Component({
   selector: 'app-edit-page',
@@ -7,9 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditPageComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+      private route: ActivatedRoute,
+      private postService: PostsService
+  ) { }
 
   ngOnInit() {
+    this.route.params.subscribe((params: Params) => {
+
+    })
   }
 
 }
