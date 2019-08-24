4b9a504ed265ab15be4a55d93dce4399e4e77df7
BakayDev <gamempvs@gmail.com>
2019-08-24 17:39:59 +0600

    Added posts output

--- src/app/admin/dashboard-page/dashboard-page.component.ts
+++ src/app/admin/dashboard-page/dashboard-page.component.ts
@@ -1,21 +1,36 @@
-import { Component, OnInit } from '@angular/core';
-import {AuthService} from "../shared/services/auth.service";
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {PostsService} from "../../shared/posts.service";
+import {Post} from "../../shared/interfaces";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-dashboard-page',
   templateUrl: './dashboard-page.component.html',
   styleUrls: ['./dashboard-page.component.scss']
 })
-export class DashboardPageComponent implements OnInit {
+export class DashboardPageComponent implements OnInit, OnDestroy {
+
+  posts: Post[] = []
+  pSub: Subscription
 
   constructor(
-      private auth: AuthService
+      private postService: PostsService
   ) { }
 
   ngOnInit() {
+   this.pSub = this.postService.getAll().subscribe(posts => {
+      this.posts = posts
+    })
   }
-  test() {
-    console.log(this.auth.token)
+
+  ngOnDestroy() {
+    if (this.pSub) {
+      this.pSub.unsubscribe()
+    }
   }
+
+    remove(id : string) {
+
+    }
 
 }
