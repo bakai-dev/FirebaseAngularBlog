ddd96bb34916f81d4761d789e305a900a25b8c54
BakayDev <gamempvs@gmail.com>
2019-08-26 02:46:08 +0600

    Localization Registration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,11 @@ import {SharedModule} from "./shared/shared.module";
 import {HTTP_INTERCEPTORS} from "@angular/common/http";
 import {AuthInterceptor} from "./shared/auth.interceptor";
 
+import ruLocale from "@angular/common/locales/ru";
+import {registerLocaleData} from "@angular/common";
+
+registerLocaleData(ruLocale,'ru')
+
 const INTERCEPTOR_PROVIDER : Provider = {
   provide: HTTP_INTERCEPTORS,
   multi: true,
